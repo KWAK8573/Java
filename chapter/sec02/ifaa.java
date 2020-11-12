5368d0cfed598fbca5b450fddf3a55ee93f53c0f
KWAK8573 <mill8573@gmail.com>
2020-11-12 10:59:10 +0900

    코멘트

---
+++ chapter/sec02/ifaa.java
@@ -0,0 +1,15 @@
+package chapter.sec02;
+
+public class ifaa {
+        public static void main(String args[]) {
+           int score =93;
+        if (score>=90){
+                System.out.println("당 멋~");
+
+                
+        }else{
+                System.out.println("환영합니다.");
+        }
+        
+        }
+}
